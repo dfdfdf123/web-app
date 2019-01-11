b9269ae55f7c7082d5f97e8574e0045d5d523d1c
johnromanoff <johnromanoff@yandex.ru>
2019-01-11 23:53:04 +0300

    init project

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject todo-list "0.1.0-SNAPSHOT"
+  :description "web app"
+  :url "https://github.com/johnromanoff/todo-list"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring "1.7.1"]]
+  :main todo-list.core)
+
